be944ff6ed1cca574de98da78d23edf020ec02f9
ghilas <1779458@crosemont.qc.ca>
2022-03-30 16:52:11 -0400

    added plugins for images and 2 salades

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
-import * as React from "react"
+import * as React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+
 import Layout from '../components/layout'
 const HomePage = () => {
   return(
     <Layout pageTitle='Home' pageHeading='Home site'>
     Welcome to my cool page
+    <StaticImage src="https://www.galbani.fr/wp-content/uploads/2020/05/AdobeStock_220126244.jpeg" 
+    alt="A salade" >
+
+    </StaticImage>
+    <StaticImage src="../images/menu_salade.jpeg" 
+    alt="A salade" >
+
+    </StaticImage>
     </Layout> 
   )
 }
