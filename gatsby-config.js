001dc06f67ee00dac3f0d8629cc615f2990831bb
ghilas <1779458@crosemont.qc.ca>
2022-03-31 17:21:55 -0400

    added itemList and images

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-  ],
+  plugins: [  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,57 @@
 import * as React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
+import itemsList from '../components/itemList';
+import {containerCard, header, card, container, photo} from "../components/layout.module.css" 
+import restaurant from "../../public/images/restaurant.png"
 
-import Layout from '../components/layout'
 const HomePage = () => {
+  
+
   return(
-    <Layout pageTitle='Home' pageHeading='Home site'>
-    Welcome to my cool page
-    <StaticImage src="https://www.galbani.fr/wp-content/uploads/2020/05/AdobeStock_220126244.jpeg" 
-    alt="A salade" >
+      <main>
+        
+          <div className={header}>
+            <h1>Welcome to MultiCuisine</h1>
+            <img src={restaurant} alt='restaurant'  />
+            <h2>Menu List</h2>
+          </div>
+        <div className={container}>
+          {itemsList.map(({id, name, price, path, desc}) => {
+            return(
+              <div className={card}>
+                <h4 key={id}>
+                  <div>
+                    <img src={path} alt={name}  className={photo}/>
+                  </div>
+                  <div className={containerCard}>
+                    <h4>
+                      <span>
+                        Name:{name}
+                      </span>
+                    </h4>
+                    <h4>
+                      <span>
+                        Price:{price}
+                      </span>  
+                    </h4>
+                    <p>
+                      <span>
+                        Descption:{desc}
+                      </span>
+                    </p>
+                  </div>
+                </h4>
+              </div>
 
-    </StaticImage>
-    <StaticImage src="../images/menu_salade.jpeg" 
-    alt="A salade" >
+            )
+           
+            }
+            )
+          }
+          </div>
+    
+      </main>
+    )
+};
 
-    </StaticImage>
-    </Layout> 
-  )
-}
 
   export default HomePage
